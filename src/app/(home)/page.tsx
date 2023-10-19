7fdb2f4d0911a121b1beccb2a0a9ed745998a022
Gerson Lamela <gersonlamela7@gmail.com>
2023-10-19 22:31:05 +0100

    feat: add third promobanner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -24,11 +24,11 @@ export default async function Home() {
   return (
     <div className="flex flex-col gap-8 py-8">
       <PromoBanner src="/banner-home-01.png" alt="até 55% desconto esse mês " />
-      <div className="mt-8 px-5">
+      <div className=" px-5">
         <Categories />
       </div>
 
-      <div className="mt-8">
+      <div>
         <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
@@ -38,10 +38,15 @@ export default async function Home() {
         alt="até 55% desconto em mouses!"
       />
 
-      <div className="mt-8">
+      <div>
         <SectionTitle>Teclados</SectionTitle>
         <ProductList products={keyboards} />
       </div>
+
+      <PromoBanner
+        src="/banner-home-03.png"
+        alt="até 55% desconto em mouses!"
+      />
     </div>
   );
 }

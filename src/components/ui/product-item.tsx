42f26737f90030b294e03b6219951daa934a2fa9
Gerson Lamela <gersonlamela7@gmail.com>
2023-10-19 22:45:06 +0100

    fix: ensure product prices are wrapped in on line

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -8,7 +8,7 @@ interface ProductItemProps {
 }
 export function ProductItem({ product }: ProductItemProps) {
   return (
-    <div className="flex max-w-[156px] flex-col gap-4">
+    <div className="flex max-w-[170px] flex-col gap-4">
       <div className="relative flex h-[170px] w-[156px]  items-center justify-center rounded-lg bg-accent">
         <Image
           src={product.imageUrls[0]}

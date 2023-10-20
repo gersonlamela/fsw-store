a2ec8f8b673483746c68b72c860990c878efdc44
Gerson Lamela <gersonlamela7@gmail.com>
2023-10-20 14:01:18 +0100

    feat: add ProductInfo component and change names files

---
+++ src/app/product/[slug]/components/ProductInfo.tsx
@@ -0,0 +1,96 @@
+"use client";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { ProductWithTotalPrice } from "@/helpers/product";
+import {
+  ArrowDown,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  TruckIcon,
+} from "lucide-react";
+import { useState } from "react";
+
+interface ProductInfoProps {
+  product: Pick<
+    ProductWithTotalPrice,
+    "basePrice" | "description" | "discountPercentage" | "totalPrice" | "name"
+  >;
+}
+export function ProductInfo({
+  product: { basePrice, description, discountPercentage, totalPrice, name },
+}: ProductInfoProps) {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleDecreaseQuantityClick = () => {
+    setQuantity((prev) => (prev === 1 ? prev : prev - 1));
+  };
+
+  const handleIncreaseQuantityClick = () => {
+    setQuantity((prev) => prev + 1);
+  };
+
+  return (
+    <div className="flex flex-col px-5 ">
+      <h2 className="text-lg">{name}</h2>
+
+      <div className="item-center flex gap-2">
+        <h1 className="text-xl font-bold">{totalPrice.toFixed(2)}€</h1>
+        {discountPercentage > 0 && (
+          <Badge className=" px-2 py-[2px]">
+            <ArrowDown size={14} /> {discountPercentage}%
+          </Badge>
+        )}
+      </div>
+
+      {discountPercentage > 0 && (
+        <p className="text-sm line-through opacity-75">
+          {Number(basePrice).toFixed(2)}€
+        </p>
+      )}
+
+      <div className="mt-4 flex items-center gap-2">
+        <Button
+          size="icon"
+          variant="outline"
+          onClick={handleDecreaseQuantityClick}
+        >
+          <ArrowLeftIcon size={16} />
+        </Button>
+
+        <span>{quantity}</span>
+
+        <Button
+          size="icon"
+          variant="outline"
+          onClick={handleIncreaseQuantityClick}
+        >
+          <ArrowRightIcon size={16} />
+        </Button>
+      </div>
+
+      <div className="mt-8 flex flex-col gap-3">
+        <h3 className="text-base font-bold">Descrição</h3>
+        <p className=" text-justify text-sm opacity-60">{description}</p>
+      </div>
+
+      <Button className="mt-8 font-bold uppercase">
+        Adicionar ao carrinho
+      </Button>
+
+      <div className="mt-5 flex  items-center justify-between rounded-lg  bg-accent px-5 py-2">
+        <div className="flex items-center gap-2">
+          <TruckIcon />
+          <div className="flex flex-col">
+            <p className="text-xs">
+              Entrega via <span className="font-bold">FSPacket®</span>
+            </p>
+            <p className="text-xs text-[#8162ff]">
+              Envio para <span className="font-bold">todo Portugal</span>
+            </p>
+          </div>
+        </div>
+        <p className="text-xs font-bold uppercase">Transporte GRÁTIS</p>
+      </div>
+    </div>
+  );
+}
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { prismaClient } from "@/lib/prisma";
 import { ProductImages } from "./components/ProductImages";
+import { ProductInfo } from "./components/ProductInfo";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 interface ProductDetailsPageProps {
   params: {
@@ -16,8 +18,9 @@ export default async function ProductDetailsPage({
 
   if (!product) return null;
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <ProductImages imageUrls={product.imageUrls} name={product.name} />
+      <ProductInfo product={computeProductTotalPrice(product)} />
     </div>
   );
 }

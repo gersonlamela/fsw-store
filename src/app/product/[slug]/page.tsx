e341b2f7625ce4ff8e653cdd05a8a7cb66b377c6
Gerson Lamela <gersonlamela7@gmail.com>
2023-10-20 15:02:33 +0100

    feat:add recommend products title to product page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { ProductImages } from "./components/ProductImages";
 import { ProductInfo } from "./components/ProductInfo";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { ProductList } from "@/components/ui/ProductList";
+import { SectionTitle } from "@/components/ui/SectionTitle";
 
 interface ProductDetailsPageProps {
   params: {
@@ -35,7 +36,11 @@ export default async function ProductDetailsPage({
     <div className="flex flex-col gap-8 pb-5">
       <ProductImages imageUrls={product.imageUrls} name={product.name} />
       <ProductInfo product={computeProductTotalPrice(product)} />
-      <ProductList products={product.category.products} />
+
+      <div>
+        <SectionTitle>PRODUTOS RECOMENDADOS</SectionTitle>
+        <ProductList products={product.category.products} />
+      </div>
     </div>
   );
 }

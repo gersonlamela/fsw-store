9c58556d3ad9ece4a2281af1a3769dd8e5fd9775
Gerson Lamela <gersonlamela7@gmail.com>
2023-10-23 00:22:23 +0100

    feat: add HOST_URL

--- src/actions/checkout.ts
+++ src/actions/checkout.ts
@@ -12,13 +12,12 @@ export const createCheckout = async (products: CartProduct[]) => {
   const checkout = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "payment",
-    success_url:
-      "https://www.google.com/search?sca_esv=575652145&sxsrf=AM9HkKkI0OeFA86QSRtOciDU_ruH-jcI-g:1698013979416&q=image+g740&tbm=isch&source=lnms&sa=X&ved=2ahUKEwj8_MOA24qCAxWSUaQEHVwkA5kQ0pQJegQICxAB&biw=842&bih=1875&dpr=1#imgrc=FwZmrDzD6gqmYM",
+    success_url: `${process.env.HOST_URL}/catalog`,
     cancel_url: process.env.HOST_URL,
     line_items: products.map((product) => {
       return {
         price_data: {
-          currency: "brl",
+          currency: "eur",
           product_data: {
             name: product.name,
             description: product.description,

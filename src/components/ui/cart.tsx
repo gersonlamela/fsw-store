7bb7365b9dbb12f2e81891a938b53cd5cd360393
Gerson Lamela <gersonlamela7@gmail.com>
2023-10-24 00:53:18 +0100

    feat: add create order server action

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { ShoppingCartIcon } from "lucide-react";
 import { Badge } from "./badge";
 import { useContext } from "react";
@@ -10,11 +11,18 @@ import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
 import { createCheckout } from "@/actions/checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { createOrder } from "@/actions/order";
+import { useSession } from "next-auth/react";
 
 export function Cart() {
+  const { data } = useSession();
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
 
   const handleFinishPurchaseClicl = async () => {
+    if (!data?.user) {
+      return;
+    }
+    await createOrder(products, (data?.user as any).id);
     const checkout = await createCheckout(products);
 
     const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);

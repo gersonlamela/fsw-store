8b29393f5ff1f5cef2ef1005a65405847e75f082
Gerson Lamela <gersonlamela7@gmail.com>
2023-10-24 12:26:29 +0100

    style: change order items text

--- src/app/orders/components/orderItem.tsx
+++ src/app/orders/components/orderItem.tsx
@@ -49,7 +49,9 @@ export function OrderItem({ order }: OrderItemProps) {
         <AccordionItem value={order.id}>
           <AccordionTrigger>
             <div className="flex flex-col gap-1 text-left">
-              <p>Pedido com {order.orderProducts.length} produto(s)</p>
+              <p className="font-bold uppercase">
+                Pedido com {order.orderProducts.length} produto(s)
+              </p>
               <span className="text-sm opacity-60">
                 Feito em {format(order.createdAt, "d/MM/y 'ás' HH:mm")}
               </span>

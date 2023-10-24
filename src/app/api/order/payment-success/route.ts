c0eaf3160d4e0866fe4c552fe971c6d846e38fe8
Gerson Lamela <gersonlamela7@gmail.com>
2023-10-24 01:08:30 +0100

    feat: ensure order is updated on checkout sucess

--- src/app/api/order/payment-success/route.ts
+++ src/app/api/order/payment-success/route.ts
@@ -1,3 +1,4 @@
+import { prismaClient } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
@@ -21,15 +22,23 @@ export const POST = async (request: Request) => {
   );
 
   if (event.type === "checkout.session.completed") {
+    const session = event.data.object as any;
     const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
       event.data.object.id,
       {
         expand: ["line_items"],
       },
     );
-    const lineItems = sessionWithLineItems.line_items;
 
-    // CRIAR PEDIDO
+    // ATUALIZAR PEDIDO
+    await prismaClient.order.update({
+      where: {
+        id: session.metadata.orderId,
+      },
+      data: {
+        status: "PAYMENT_CONFIRMED",
+      },
+    });
   }
 
   return NextResponse.json({ received: true });
--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -22,8 +22,8 @@ export function Cart() {
     if (!data?.user) {
       return;
     }
-    await createOrder(products, (data?.user as any).id);
-    const checkout = await createCheckout(products);
+    const order = await createOrder(products, (data?.user as any).id);
+    const checkout = await createCheckout(products, order.id);
 
     const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
